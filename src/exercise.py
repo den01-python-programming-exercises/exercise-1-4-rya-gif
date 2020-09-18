93b184deaf061c0c48797a86277e13eb414c7e85
ryagif <21254@bridgend.ac.uk>
2020-09-18 12:20:17 +0000

    yo

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,6 @@
 def main():
     #write your code below this line
-    message = input("Write a message...")
-
+    message = input("bye")
+    message = input ("once upon a time")
 if __name__ == '__main__':
     main()
